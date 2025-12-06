bc9bce1a2df4ad1eb0de9224b8058cf8d134134d
Shonte Monson <taymontech@gmail.com>
2025-12-06 04:37:48 -0500

    Customer Seed Data Addition

--- BytePizzaPrototype/BytePizza/Data/SeedData.cs
+++ BytePizzaPrototype/BytePizza/Data/SeedData.cs
@@ -1,14 +1,39 @@
 ﻿///Seeds the database with demo users at runtime. Add default customer login combinations methods here.
 
+using Microsoft.EntityFrameworkCore;
+using BytePizza.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace BytePizza.Data
 {
-    internal class SeedData
+    public static class SeedData
     {
+        ///<summary>
+        ///Customer accounts seeded into database
+        ///</summary>
+        public static void Initialize(ApplicationDBContext context) 
+        { 
+            context.Database.EnsureCreated();
+            //Checks for seeded data within database to prevent duplication
+            if (context.Customers.Any())
+            {
+                return;
+            }
+            //Seed database with test customers (login with phone numbers & passwords)
+            var customers = new List<Customer>
+            {
+                new Customer
+                {
+                    Phone = "5551234567",
+                    Password = "pizza",
+                    Name = "John Doe",
+                    Address = "123 Main Street"
+                }
+            };
+            context.Customers.AddRange(customers);
+            context.SaveChanges();
+        }
     }
 }
