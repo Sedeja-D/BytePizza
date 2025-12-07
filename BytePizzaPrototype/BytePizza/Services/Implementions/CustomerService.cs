b93d77d5f7161b44aec6e3846a33202884615903
Shonte Monson <taymontech@gmail.com>
2025-12-06 21:25:12 -0500

    Added a bit of logic into the ICustomerService interface and CustomerService implementation class. Implementation code in CustomerService is a placeholder just to remove errors due to lack of implementation.

--- BytePizzaPrototype/BytePizza/Services/Implementions/CustomerService.cs
+++ BytePizzaPrototype/BytePizza/Services/Implementions/CustomerService.cs
@@ -1,13 +1,21 @@
 ﻿///Details what the services need to do but not how it needs to be done.
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using BytePizza.Models;
+using BytePizza.Data;
+using BytePizza.Services.Interfaces;
 
 namespace BytePizza.Services.Implementions
 {
-    internal class CustomerService
+    public class CustomerService : ICustomerService
     {
+        public Task<Customer?> GetCustomerAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> LoginAsync(string phone, string password)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
--- BytePizzaPrototype/BytePizza/Services/Interfaces/ICustomerService.cs
+++ BytePizzaPrototype/BytePizza/Services/Interfaces/ICustomerService.cs
@@ -1,15 +1,20 @@
 ﻿///Tells the system WHAT the customer data needs to do. Tasks: load menu items and their prices from the database, get items by category 
 ///such as size, crust, toppings and beverages. Tasks: Authenticate customers via login and logout and verify 
 ///customer details against database records.
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using BytePizza.Models;
 
 namespace BytePizza.Services.Interfaces
 {
-    internal interface ICustomerService
+    public interface ICustomerService
     {
+        ///<summary>
+        ///Customer phone number and passwrod used for authentication.
+        ///</summary>
+        Task<bool> LoginAsync(string phone, string password);
+
+        ///<summary>
+        ///Form of state management representing currently authenticated customer
+        ///</summary>
+        Task<Customer?> GetCustomerAsync();
     }
 }
