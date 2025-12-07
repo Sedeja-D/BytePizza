620ca7d8d559910ae27b820f319293ecc35856da
Rowan Drake <RowanDrake04@gmail.com>
2025-12-07 13:52:18 -0500

    Added CustomerService with Login, Logout, and GetCustomer

--- BytePizzaPrototype/BytePizza/Services/Implementions/CustomerService.cs
+++ BytePizzaPrototype/BytePizza/Services/Implementions/CustomerService.cs
@@ -3,19 +3,66 @@ using Microsoft.EntityFrameworkCore;
 using BytePizza.Models;
 using BytePizza.Data;
 using BytePizza.Services.Interfaces;
+using System.Diagnostics.Eventing.Reader;
 
 namespace BytePizza.Services.Implementions
 {
     public class CustomerService : ICustomerService
     {
-        public Task<Customer?> GetCustomerAsync()
+        private readonly ApplicationDBContext _context;
+
+        //storing ID in memory
+        private static int? CustomerID = null;
+
+        public CustomerService(ApplicationDBContext context)
         {
-            throw new NotImplementedException();
+            _context = context;
         }
 
-        public Task<bool> LoginAsync(string phone, string password)
+        //get logged in customerID, returns null for logged out.
+        public async Task<Customer?> GetCustomerAsync()
         {
-            throw new NotImplementedException();
+            if (CustomerID == null)
+            {
+                //not logged in
+                return null;
+            }
+
+            
+            else
+            {
+                //wait for DB response and return ID
+                return await _context.Customers.FirstOrDefaultAsync(c => c.CustomerId == CustomerID.Value);
+            }  
+        }
+
+        //Auth customer login with PN + PW; stores CustomerID on success.
+        public async Task<bool> LoginAsync(string phone, string password)
+        {
+            //wait for DB response for phone
+            var customer = await _context.Customers.FirstOrDefaultAsync(c => c.Phone == phone);
+
+            if (customer == null)
+            {
+                //if phone doesn't exist, return false.
+                return false;
+            }
+
+            if (customer.Password != password)
+            {
+                //check for correct password for given PN
+                return false;
+            }
+
+            //logged in; set CustomerID in memory
+            CustomerID = customer.CustomerId;
+            return true;
+        }
+
+        //wipes CustomerID
+        public void Logout()
+        {
+            CustomerID = null;
         }
     }
 }
--- BytePizzaPrototype/BytePizza/Services/Interfaces/ICustomerService.cs
+++ BytePizzaPrototype/BytePizza/Services/Interfaces/ICustomerService.cs
@@ -1,20 +1,29 @@
-﻿///Tells the system WHAT the customer data needs to do. Tasks: load menu items and their prices from the database, get items by category 
-///such as size, crust, toppings and beverages. Tasks: Authenticate customers via login and logout and verify 
-///customer details against database records.
+﻿///Tells the system WHAT the customer data needs to do.
+///Tasks: Authenticate customers via login and logout and verify customer details against database records.
 using BytePizza.Models;
 
 namespace BytePizza.Services.Interfaces
 {
     public interface ICustomerService
     {
+
         ///<summary>
-        ///Customer phone number and passwrod used for authentication.
+        ///Logs in customer using Phone + Pass
         ///</summary>
+        ///<param name="phone">10 Digit Number</param>
+        ///<param name="password">User Password</param>
+        ///<returns> True for login, False for incorrect info</returns>
         Task<bool> LoginAsync(string phone, string password);
 
         ///<summary>
         ///Form of state management representing currently authenticated customer
         ///</summary>
+        ///<returns> CustomerID or Null for logged out </returns>
         Task<Customer?> GetCustomerAsync();
+
+        ///<summary>
+        ///Logout customer; wiping CustomerID
+        ///</summary>
+        void Logout();
     }
 }
